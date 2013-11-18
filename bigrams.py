16f1170bc005451f1a789e916ae9a0c4ff8cc01f
tim <tim@rewire.it>
2013-11-18 00:31:10 +0000

    tidyied script

--- bigrams.py
+++ bigrams.py
@@ -91,28 +91,6 @@ def main(corpus=None):
         if istext(f):
             CORPUS += f
 
-    #meta = u.info()
-    #file_size = int(meta.getheaders("Content-Length")[0])
-    #print("Downloading: {0} Bytes: {1}".format(url, file_size))
-    #
-    #file_size_dl = 0
-    #block_sz = 8192
-    #while True:
-    #    buffer = u.read(block_sz)
-    #    if not buffer:
-    #        break
-    #
-    #    file_size_dl += len(buffer)
-    #    f.write(buffer)
-    #    p = float(file_size_dl) / file_size
-    #    status = r"{0}  [{1:.2%}]".format(file_size_dl, p)
-    #    status = status + chr(8)*(len(status)+1)
-    #    sys.stdout.write(status)
-
-    #f.close()
-
-
-
     # ########################
 
     # add words from Brown corpus to our Corpus
